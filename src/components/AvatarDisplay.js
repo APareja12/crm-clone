cf7b5ce03fa78a62b848286cbd55e2a85222d286
APareja12 <alexis.pareja@hotmail.com>
2022-07-12 13:23:52 -0400

    creates avatar display

--- src/components/AvatarDisplay.js
+++ src/components/AvatarDisplay.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import blankAvatar from '../images/blank-profile.png'
 
-const AvatarDisplay = () => {
+const AvatarDisplay = ({ ticket }) => {
   return (
-    <div>
-      AvatarDisplay
-    </div>
+<div className="avatar-container">
+<div className="img-container">
+<img src={ticket.avatar ? ticket.avatar : blankAvatar} alt={'photo of ' + ticket.owner} />
+</div>
+</div>
   )
 }
 
