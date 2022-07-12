04a4be04a5af7a8161a7497bbf53590d391b488c
APareja12 <alexis.pareja@hotmail.com>
2022-07-12 09:26:12 -0400

    adds nav component and logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import logo from '../images/crm-logo.png'
 
 const Nav = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <div className="logo-container" onClick={() => navigate('/')}>
+        <img src={logo} alt="logo"/>
+      </div>
+      <div className="controls-container">
+        <div className="icon" onClick={() => navigate('/ticket')}>＋</div>
+        <div className="icon" onClick={() => navigate('/')}>❮❮</div>
+      </div>
+    </nav>
   )
 }
 
